b5ed0982bab10f33184ca223873cdbb3464a4a9d
santoshkalukunda <santoshkalukunda@gmail.com>
2023-09-03 17:14:42 +0545

    some chages

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -2,14 +2,14 @@
     <div class="container-fluid">
         <a href="{{route('index')}}" class="navbar-brand">
             <img src="{{ appSettings('logo') ? asset('storage/' . appSettings('logo')) : asset('assets/img/no-image.png') }}" alt="{{appSettings('site_name')}}" >
-           {{appSettings('site_name')}}
+           {{-- {{appSettings('site_name')}} --}}
         </a>
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-         <x-frontend.category-menu-view />
+            <x-frontend.category-menu-view />
         </div>
     </div>
 </div>
