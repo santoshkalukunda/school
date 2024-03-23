3398d39b3584b4d214561faf9078f81724077676
santoshkalukunda <santoshkalukunda@gmail.com>
2024-03-23 21:33:30 +0545

    page component

--- resources/views/components/frontend/page-component.blade.php
+++ resources/views/components/frontend/page-component.blade.php
@@ -15,7 +15,7 @@
                     </div>
                 </div>
             @else
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
                     <h1 class="mb-3">{{ $page->title }}</h1>
                     {!! $page->descriptions !!}
                 </div>
